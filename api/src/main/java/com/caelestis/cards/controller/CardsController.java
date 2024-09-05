614a66884fc903b948f137b088c6af73174ce53b
Caelest1s <jefferson.crn@gmail.com>
2024-09-05 18:03:08 -0300

    feat: Endpoint createCard

---
+++ api/src/main/java/com/caelestis/cards/controller/CardsController.java
@@ -0,0 +1,26 @@
+package com.caelestis.cards.controller;
+
+import com.caelestis.cards.constants.CardsConstants;
+import com.caelestis.cards.dto.ResponseDto;
+import com.caelestis.cards.service.ICardsService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping(path = "/api", produces = {MediaType.APPLICATION_JSON_VALUE})
+@AllArgsConstructor
+public class CardsController {
+
+    ICardsService iCardsService;
+
+    @PostMapping(value = "/create")
+    public ResponseEntity<ResponseDto> createCard(@RequestParam String mobileNumber){
+
+        iCardsService.createCard(mobileNumber);
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(new ResponseDto(CardsConstants.STATUS_201, CardsConstants.MESSAGE_201));
+    }
+}
