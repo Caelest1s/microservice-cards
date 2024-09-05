3c7dbe84a4a015525b8cb427cf4c7af18776afdf
Caelest1s <jefferson.crn@gmail.com>
2024-09-05 11:56:21 -0300

    feat: Add class BaseEntity and Cards

---
+++ api/src/main/java/com/caelestis/cards/entity/Cards.java
@@ -0,0 +1,28 @@
+package com.caelestis.cards.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+
+@Entity
+@Getter @Setter @ToString @AllArgsConstructor @NoArgsConstructor
+public class Cards extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long card_id;
+
+    private String mobileNumber;
+
+    private String cardNumber;
+
+    private String cardType;
+
+    private Integer totalLimit;
+
+    private Integer amountUsed;
+
+    private Integer availableAmount;
+}
